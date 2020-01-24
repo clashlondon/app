928a15f3ee6b2de0910115ec8e2ffc95a2ff31b2
MBP <mbp@mbps-mbp.home>
2020-01-24 15:32:23 +0000

    Updates

---
+++ _next/static/P5KvsMAcYZbVjqpQ9HJAl/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = (function(a){return {"/":[a],"/index":[a],"/war":[a]}}("static\u002Fchunks\u002Fd12e1fe63b7410f5559c59c6f54c99486beff906.18b039a2f6e48da3fb26.js"));self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
